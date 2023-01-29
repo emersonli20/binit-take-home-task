84404ada0e2599eafc445eb197495bc2d86a04cd
emersonli20 <61331273+emersonli20@users.noreply.github.com>
2023-01-29 16:17:17 -0500

    color 2 classes in plot; modify umap parameters

--- plot.py
+++ plot.py
@@ -7,47 +7,50 @@ import umap.umap_ as umap
 from matplotlib import pyplot as plt
 import typer
 import os
+from tqdm import tqdm
 
 def main(directory: str):
+    # create model
     m = timm.create_model('resnet50', pretrained=True)
 
-    transform = transforms.ToTensor()
-
     # create training dataset
+    transform = transforms.ToTensor()
     trainset = CIFAR10(root='./data', train=True, download=True, transform=transform)
 
+    # choose 2 classes in CIFAR10
+    # automobile
     idx1 = torch.tensor(trainset.targets) == 1
-    idx8 = torch.tensor(trainset.targets) == 8
-    train_mask = idx1 | idx8
+    # frog
+    idx2 = torch.tensor(trainset.targets) == 6
+    train_mask = idx1 | idx2
     train_indices = train_mask.nonzero().reshape(-1)
 
+    # take 1500 image subset of CIFAR10 that only contains automobile and frog
     train_subset = Subset(trainset, train_indices)
 
-    image, label = train_subset[0]
-    image = torch.unsqueeze(image, dim=0)
-    image2, label2 = train_subset[1]
-    image2 = torch.unsqueeze(image2, dim=0)
+    labels = []
+    images, label = train_subset[0]
+    labels.append(label)
 
-    images, _ = train_subset[0]
     images = torch.unsqueeze(images, dim=0)
-    for i in range(1,100):
-        new_image, _ = train_subset[i]
+    for i in tqdm(range(1,1500)):
+        new_image, label = train_subset[i]
+        labels.append(label)
         new_image = torch.unsqueeze(new_image, dim=0)
         images = torch.cat((images, new_image), dim=0)
 
-
+    # extract features from penultimate layer
     o = m.forward_features(images)
-
     o_flat = torch.flatten(o, start_dim=1)
-
     data = o_flat.detach().numpy()
 
-    reducer = umap.UMAP(random_state=42)
+    # reduce the features to a 2d embedding space
+    reducer = umap.UMAP(n_neighbors=7, min_dist=0.025, random_state=42)
     reducer.fit(data)
-
     embedding = reducer.transform(data)
 
-    plt.scatter(embedding[:,0], embedding[:,1])
+    # plot 2d embedding space
+    plt.scatter(embedding[:,0], embedding[:,1], c=labels)
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, "embeddings.png")
     plt.savefig(path)
